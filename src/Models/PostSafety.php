3e42cf823b532ecaa9c88cb1f7cc1e13f1fd9851
Marcin Kurczewski <mkurczew@gmail.com>
2013-10-08 23:00:19 +0200

    Removed unnecessary method

--- src/Models/PostSafety.php
+++ src/Models/PostSafety.php
@@ -4,9 +4,4 @@ class PostSafety extends Enum
 	const Safe = 1;
 	const Sketchy = 2;
 	const Unsafe = 3;
-
-	public static function getAll()
-	{
-		return [self::Safe, self::Sketchy, self::Unsafe];
-	}
 }
