cb18928cbb5706c75d759eeb2876db6acee857e6
Mariusz Dziemianowicz <mariusz.dziemianowicz@gmail.com>
2013-10-09 18:55:58 +0200

    Fixes #16: Add jquery and google fonts locally

--- init.php
+++ init.php
@@ -7,5 +7,17 @@ function download($source, $destination)
 	file_put_contents($destination, $content);
 }
 
-download('http://raw.github.com/aehlke/tag-it/master/css/jquery.tagit.css', './public_html/media/css/jquery.tagit.css');
-download('http://raw.github.com/aehlke/tag-it/master/js/tag-it.min.js', './public_html/media/js/jquery.tagit.js');
+$path = './public_html/';
+
+//jQuery
+download('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', $path . 'media/js/jquery.min.js');
+download('http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js', $path . 'media/js/jquery-ui.min.js');
+download('http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css', $path . 'media/css/jquery-ui.css');
+
+//jQuery Tag-it!
+download('http://raw.github.com/aehlke/tag-it/master/css/jquery.tagit.css', $path . 'media/css/jquery.tagit.css');
+download('http://raw.github.com/aehlke/tag-it/master/js/tag-it.min.js', $path . 'media/js/jquery.tagit.js');
+
+//fonts
+download('http://googlefontdirectory.googlecode.com/hg/apache/droidsans/DroidSans.ttf', $path . 'media/fonts/DroidSans.ttf');
+download('http://googlefontdirectory.googlecode.com/hg/apache/droidsans/DroidSans-Bold.ttf', $path . 'media/fonts/DroidSans-Bold.ttf');
