c6cdc1d9450c14cf2d4d5a60485c1832a4a6cf88
Marcin Kurczewski <mkurczew@gmail.com>
2013-10-14 09:51:38 +0200

    Fixed counting occurences of numeric keys

--- src/Controllers/TagController.php
+++ src/Controllers/TagController.php
@@ -16,19 +16,16 @@ class TagController
 		$dbQuery->innerJoin('post_tag');
 		$dbQuery->on('tag.id = post_tag.tag_id');
 		$dbQuery->groupBy('tag.id');
+		$dbQuery->orderBy('count desc, LOWER(tag.name) asc');
 		$rows = $dbQuery->get();
 
 		$tags = [];
 		$tagDistribution = [];
 		foreach ($rows as $row)
 		{
-			$tags []= $row['name'];
-			$tagDistribution[$row['name']] = $row['count'];
+			$tags []= strval($row['name']);
+			$tagDistribution[$row['name']] = intval($row['count']);
 		}
-		array_multisort(
-			array_values($tagDistribution), SORT_DESC, SORT_NUMERIC,
-			array_keys($tagDistribution), SORT_ASC, SORT_NATURAL | SORT_FLAG_CASE,
-			$tagDistribution);
 
 		$this->context->transport->tags = $tags;
 		$this->context->transport->tagDistribution = $tagDistribution;
