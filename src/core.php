6dd61638332fa489bd84fa3e6726c7c5d10986f6
Marcin Kurczewski <mkurczew@gmail.com>
2013-10-16 19:45:57 +0200

    Added SQL query count in footer

--- src/core.php
+++ src/core.php
@@ -10,6 +10,7 @@ trueStartTime();
 
 require_once 'lib/redbean/RedBean/redbean.inc.php';
 require_once 'lib/chibi-core/Facade.php';
+require_once 'lib/chibi-core/Registry.php';
 
 date_default_timezone_set('UTC');
 setlocale(LC_CTYPE, 'en_US.UTF-8');
@@ -48,3 +49,12 @@ R::dependencies(['tag' => ['post'], 'favoritee' => ['post', 'user']]);
 foreach (\Chibi\AutoLoader::getAllIncludablePaths() as $path)
 	if (preg_match('/Model/', $path))
 		\Chibi\AutoLoader::safeInclude($path);
+
+function queryLogger()
+{
+	static $queryLogger = null;
+	if ($queryLogger === null)
+		$queryLogger = RedBean_Plugin_QueryLogger::getInstanceAndAttach(R::getDatabaseAdapter());
+	return $queryLogger;
+}
+queryLogger();
