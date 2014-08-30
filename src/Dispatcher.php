4202ae2ec73242d108996f8841d9a52c8604bb06
Marcin Kurczewski <mkurczew@gmail.com>
2014-08-30 22:16:00 +0200

    Added HTTP response codes

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -19,10 +19,12 @@ final class Dispatcher
 		global $start;
 		try
 		{
+			$code = 200;
 			$json = $this->router->handle($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 		}
 		catch (\Exception $e)
 		{
+			$code = 400;
 			$json = [
 				'error' => $e->getMessage(),
 				'trace' => $e->getTrace(),
@@ -30,6 +32,8 @@ final class Dispatcher
 		}
 		$end = microtime(true);
 		$json['__time'] = $end - $start;
+
+		http_response_code($code);
 		header('Content-Type: application/json');
 		echo json_encode($json);
 	}
