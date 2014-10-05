91876bb66f5ca9fa9df51a5f438eabb199015bbe
Marcin Kurczewski <mkurczew@gmail.com>
2014-10-05 21:07:48 +0200

    Reversed default history order

--- src/SearchServices/Filters/SnapshotFilter.php
+++ src/SearchServices/Filters/SnapshotFilter.php
@@ -3,6 +3,13 @@ namespace Szurubooru\SearchServices\Filters;
 
 class SnapshotFilter extends BasicFilter implements IFilter
 {
+	const ORDER_ID = 'id';
+
 	const REQUIREMENT_PRIMARY_KEY = 'primaryKey';
 	const REQUIREMENT_TYPE = 'type';
+
+	public function __construct()
+	{
+		$this->setOrder([self::ORDER_ID => self::ORDER_DESC]);
+	}
 }
