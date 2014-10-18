e6fc010897631a5737e40e6ba050e37a4f9a877a
Marcin Kurczewski <mkurczew@gmail.com>
2014-10-18 14:29:26 +0200

    Improved message when renaming tag to existing one

--- src/Services/TagService.php
+++ src/Services/TagService.php
@@ -173,6 +173,9 @@ class TagService
 
 	private function updateTagName(Tag $tag, $newName)
 	{
+		$otherTag = $this->tagDao->findByName($newName);
+		if ($otherTag and $otherTag->getId() !== $tag->getId())
+			throw new \DomainException('Tag with this name already exists.');
 		$tag->setName($newName);
 	}
 
