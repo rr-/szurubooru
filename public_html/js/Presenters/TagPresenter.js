rr-/szurubooru

commit b3def7fc21cc6f72ebf2217976614fe383e24211

Improved API responses

public_html/js/Presenters/TagPresenter.js
@@ -66,9 +66,9 @@ App.Presenters.TagPresenter = function(
                 api.get('tags/' + tagName + '/siblings'),
                 api.get('posts', {query: tagName}))
             .then(function(tagResponse, siblingsResponse, postsResponse) {
-                tag = tagResponse.json;
-                siblings = siblingsResponse.json.data;
-                posts = postsResponse.json.data;
+                tag = tagResponse.json.tag;
+                siblings = siblingsResponse.json.tags;
+                posts = postsResponse.json.posts;
                 posts = posts.slice(0, 8);
 
                 render();

public_html/js/Controls/TagInput.js
@@ -387,7 +387,7 @@ App.Controls.TagInput = function($underlyingInput) {
         return promise.make(function(resolve, reject) {
             promise.wait(api.get('/tags/' + tagName + '/siblings'))
                 .then(function(response) {
-                    resolve(response.json.data);
+                    resolve(response.json.tags);
                 }).fail(function() {
                     reject();
                 });

public_html/js/Pager.js
@@ -71,10 +71,7 @@ App.Pager = function(
                     var totalRecords = response.json.totalRecords;
                     totalPages = Math.ceil(totalRecords / pageSize);
 
-                    resolve({
-                        entities: response.json.data,
-                        totalRecords: totalRecords,
-                        totalPages: totalPages});
+                    resolve(response);
 
                 }).fail(function(response) {
                     reject(response);

public_html/js/Presenters/CommentListPresenter.js
@@ -53,7 +53,7 @@ App.Presenters.CommentListPresenter = function(
                     if (comments.length === 0) {
                         promise.wait(api.get('/comments/' + params.post.id))
                             .then(function(response) {
-                                comments = response.json.data;
+                                comments = response.json.comments;
                                 render();
                             }).fail(function() {
                                 console.log(arguments);
@@ -176,7 +176,7 @@ App.Presenters.CommentListPresenter = function(
 
         p.then(function(response) {
             $textarea.val('');
-            var comment = response.json;
+            var comment = response.json.comment;
 
             if (commentToEdit) {
                 $form.slideUp(function() {

public_html/js/Presenters/GlobalCommentListPresenter.js
@@ -38,8 +38,8 @@ App.Presenters.GlobalCommentListPresenter = function(
                         baseUri: '#/comments',
                         backendUri: '/comments',
                         $target: $el.find('.pagination-target'),
-                        updateCallback: function($page, data) {
-                            renderComments($page, data.entities);
+                        updateCallback: function($page, response) {
+                            renderComments($page, response.json.comments);
                         },
                     },
                     function() {
@@ -65,12 +65,11 @@ App.Presenters.GlobalCommentListPresenter = function(
         $el.html(templates.list());
     }
 
-    function renderComments($page, data) {
+    function renderComments($page, postComments) {
         var $target = $page.find('.posts');
-        _.each(data, function(data) {
-            var post = data.post;
-            var comments = data.comments;
-
+        _.each(postComments, function(postComments) {
+            var post = postComments.post;
+            var comments = postComments.comments;
             var $post = jQuery('<li>' + templates.listItem({
                 util: util,
                 post: post,

public_html/js/Presenters/HistoryPresenter.js
@@ -31,8 +31,8 @@ App.Presenters.HistoryPresenter = function(
                         baseUri: '#/history',
                         backendUri: '/history',
                         $target: $el.find('.pagination-target'),
-                        updateCallback: function($page, data) {
-                            renderHistory($page, data.entities);
+                        updateCallback: function($page, response) {
+                            renderHistory($page, response.json.history);
                         },
                     },
                     function() {

public_html/js/Presenters/PagerPresenter.js
@@ -114,7 +114,14 @@ App.Presenters.PagerPresenter = function(
                     updateCallback($page, response);
 
                     refreshPageList();
-                    if (!response.entities.length) {
+
+                    var entities =
+                        response.json.posts ||
+                        response.json.users ||
+                        response.json.comments ||
+                        response.json.tags;
+
+                    if (!entities.length) {
                         messagePresenter.showInfo($messages, 'No data to show');
                         if (pager.getVisiblePages().length === 1) {
                             hidePageList();
@@ -125,7 +132,7 @@ App.Presenters.PagerPresenter = function(
                         showPageList();
                     }
 
-                    if (pager.getPage() < response.totalPages) {
+                    if (pager.getPage() < pager.getTotalPages()) {
                         attachNextPageLoader();
                     }
 

public_html/js/Presenters/PostEditPresenter.js
@@ -151,8 +151,9 @@ App.Presenters.PostEditPresenter = function(
         promise.wait(api.post('/posts/' + post.id, formData))
             .then(function(response) {
                 tagList.refreshTags();
+                post = response.json.post;
                 if (typeof(updateCallback) !== 'undefined') {
-                    updateCallback(post = response.json);
+                    updateCallback(post);
                 }
             }).fail(function(response) {
                 showEditError(response);

commit 7ca582186b26575ebececeae4148a083a4fcdceb

Fixed redirection after tag editing
Regression from b3def7f.

public_html/js/Presenters/TagPresenter.js
@@ -126,7 +126,7 @@ App.Presenters.TagPresenter = function(
 
         promise.wait(api.put('/tags/' + tag.name, formData))
             .then(function(response) {
-                router.navigateInplace('#/tag/' + response.json.name);
+                router.navigateInplace('#/tag/' + response.json.tag.name);
                 tagList.refreshTags();
             }).fail(function(response) {
                 window.alert(response.json && response.json.error || 'An error occured.');