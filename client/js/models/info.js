611cb3b9f901e1b0a59c8ea1f77a04fd3ca10e3f
rr- <rr-@sakuya.pl>
2016-07-30 13:35:19 +0200

    client/home: fix post not being bound to its model
    This resulted in errors while presenting post content control - it tried
    to use methods that were available only in the actual model.

--- client/js/models/info.js
+++ client/js/models/info.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const api = require('../api.js');
+const Post = require('./post.js');
 
 class Info {
     static get() {
         return api.get('/info')
             .then(response => {
+                if (response.featuredPost) {
+                    response.featuredPost =
+                        Post.fromResponse(response.featuredPost);
+                }
                 return Promise.resolve(response);
             }, response => {
                 return Promise.reject(response.errorMessage);
