rr-/szurubooru

commit 9aea55e3d10188cec8d1926704610a2567ca718a

server/general: embrace most of PEP8
Ignored only the rules about continuing / hanging indentation.

Also, added __init__.py to tests so that pylint discovers them. (I don't
buy pytest's BS about installing your package.)

server/szurubooru/migrations/versions/ed6dd16a30f3_delete_post_columns.py
@@ -13,24 +13,36 @@ down_revision = '46df355634dc'
 branch_labels = None
 depends_on = None
 
+
 def upgrade():
-    op.drop_column('post', 'auto_comment_edit_time')
-    op.drop_column('post', 'auto_fav_count')
-    op.drop_column('post', 'auto_comment_creation_time')
-    op.drop_column('post', 'auto_feature_count')
-    op.drop_column('post', 'auto_comment_count')
-    op.drop_column('post', 'auto_score')
-    op.drop_column('post', 'auto_fav_time')
-    op.drop_column('post', 'auto_feature_time')
-    op.drop_column('post', 'auto_note_count')
+    for column_name in [
+            'auto_comment_edit_time'
+            'auto_fav_count',
+            'auto_comment_creation_time',
+            'auto_feature_count',
+            'auto_comment_count',
+            'auto_score',
+            'auto_fav_time',
+            'auto_feature_time',
+            'auto_note_count']:
+        op.drop_column('post', column_name)
+
 
 def downgrade():
-    op.add_column('post', sa.Column('auto_note_count', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_feature_time', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_fav_time', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_score', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_comment_count', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_feature_count', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_comment_creation_time', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_fav_count', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('post', sa.Column('auto_comment_edit_time', sa.INTEGER(), autoincrement=False, nullable=False))
+    for column_name in [
+            'auto_note_count',
+            'auto_feature_time',
+            'auto_fav_time',
+            'auto_score',
+            'auto_comment_count',
+            'auto_feature_count',
+            'auto_comment_creation_time',
+            'auto_fav_count',
+            'auto_comment_edit_time']:
+        op.add_column(
+            'post',
+            sa.Column(
+                column_name,
+                sa.INTEGER(),
+                autoincrement=False,
+                nullable=False))

commit ff7bbbdd8a7fc24a17c93393436d622dc1e4ae4c

server/db: fix upgrade (regression from 9aea55e)

server/szurubooru/migrations/versions/ed6dd16a30f3_delete_post_columns.py
@@ -16,7 +16,7 @@ depends_on = None
 
 def upgrade():
     for column_name in [
-            'auto_comment_edit_time'
+            'auto_comment_edit_time',
             'auto_fav_count',
             'auto_comment_creation_time',
             'auto_feature_count',