rr-/szurubooru

commit d5e197e6ea353e1d635c33b95cd342d51c5baf76

client/general: add empty href for link buttons
In e464e69 I removed href='#' but I noticed that it broke some things.
Readding href serves two purposes:

- it makes links reachable with Tab key
- it makes links clickable with Enter key

The alternative to this approach was to introduce [tabindex] and [role]
attributes. But not only using tabindex=0 with <a/> is questionable,
it'd require adding a keyboard handler that'd intercept space and return
key presses and simulated link clicks. Since it's best to leave this
kind of thing to the native UI, I went with readding hrefs instead. I
believe that hash hrefs, even though being a common practice, are silly,
so I decided to settle down with empty hrefs.

As a bonus, I added a snippet that prevents middle mouse clicks from
opening such links/buttons in new tabs, which was the motivation for
e464e69.

client/js/controls/auto_complete_control.js
@@ -215,6 +215,7 @@ class AutoCompleteControl {
             const listItem = document.createElement('li');
             const link = document.createElement('a');
             link.innerHTML = resultItem.caption;
+            link.setAtribute('href', '');
             link.setAttribute('data-key', resultItem.value);
             link.addEventListener(
                 'mouseenter',

client/js/controls/tag_input_control.js
@@ -251,6 +251,7 @@ class TagInputControl extends events.EventTarget {
     }
 
     _evtAddTagButtonClick(e) {
+        e.preventDefault();
         this.addTag(this._tagInputNode.value, SOURCE_USER_INPUT);
         this._tagInputNode.value = '';
     }
@@ -324,6 +325,7 @@ class TagInputControl extends events.EventTarget {
 
         const removalLinkNode = document.createElement('a');
         removalLinkNode.classList.add('append');
+        removalLinkNode.setAttribute('href', '')
         removalLinkNode.setAttribute('data-pseudo-content', '×');
         removalLinkNode.addEventListener('click', e => {
             e.preventDefault();
@@ -387,6 +389,7 @@ class TagInputControl extends events.EventTarget {
             const addLinkNode = document.createElement('a');
             addLinkNode.textContent = tagName;
             addLinkNode.classList.add('add-tag');
+            addLinkNode.setAttribute('href', '');
             if (actualTag) {
                 addLinkNode.classList.add(
                     misc.makeCssName(actualTag.category, 'tag'));
@@ -405,6 +408,7 @@ class TagInputControl extends events.EventTarget {
             const removeLinkNode = document.createElement('a');
             removeLinkNode.classList.add('remove-tag');
             removeLinkNode.classList.add('append');
+            removeLinkNode.setAttribute('href', '');
             removeLinkNode.setAttribute('data-pseudo-content', '×');
             removeLinkNode.addEventListener('click', e => {
                 e.preventDefault();

client/html/comment.tpl
@@ -32,13 +32,13 @@
             --><span class='score-container'></span><!--
 
             --><% if (ctx.canEditComment) { %><!--
-                --><a class='edit'><!--
+                --><a href class='edit'><!--
                     --><i class='fa fa-pencil'></i> edit<!--
                 --></a><!--
             --><% } %><!--
 
             --><% if (ctx.canDeleteComment) { %><!--
-                --><a class='delete'><!--
+                --><a href class='delete'><!--
                     --><i class='fa fa-remove'></i> delete<!--
                 --></a><!--
             --><% } %><!--

client/html/comment_form.tpl
@@ -16,8 +16,8 @@
 
         <nav class='buttons'>
             <ul>
-                <li class='preview'><a>Preview</a></li>
-                <li class='edit'><a>Edit</a></li>
+                <li class='preview'><a href>Preview</a></li>
+                <li class='edit'><a href>Edit</a></li>
             </ul>
         </nav>
 

client/html/expander.tpl
@@ -1,6 +1,6 @@
 <section class='expander'>
     <header>
-        <a>
+        <a href>
             <%- ctx.title %>
             <i class='fa fa-chevron-down'></i>
         </a>

client/html/fav.tpl
@@ -1,9 +1,9 @@
 <% if (ctx.canFavorite) { %>
     <% if (ctx.ownFavorite) { %>
-        <a class='remove-favorite'>
+        <a href class='remove-favorite'>
             <i class='fa fa-heart'></i>
     <% } else { %>
-        <a class='add-favorite'>
+        <a href class='add-favorite'>
             <i class='fa fa-heart-o'></i>
     <% } %>
 <% } else { %>

client/html/post_edit_sidebar.tpl
@@ -57,9 +57,9 @@
 
         <% if (ctx.canEditPostNotes) { %>
             <section class='notes'>
-                <a class='add'>Add a note</a>
+                <a href class='add'>Add a note</a>
                 <%= ctx.makeTextarea({disabled: true, text: 'Content (supports Markdown)', rows: '8'}) %>
-                <a class='delete inactive'>Delete selected note</a>
+                <a href class='delete inactive'>Delete selected note</a>
             </section>
         <% } %>
 
@@ -74,7 +74,7 @@
             <section class='post-thumbnail'>
                 <label>Thumbnail</label>
                 <div class='dropper-container'></div>
-                <a>Discard custom thumbnail</a>
+                <a href>Discard custom thumbnail</a>
             </section>
         <% } %>
 
@@ -82,10 +82,10 @@
             <section class='management'>
                 <ul>
                     <% if (ctx.canFeaturePosts) { %>
-                        <li><a class='feature'>Feature this post on main page</a></li>
+                        <li><a href class='feature'>Feature this post on main page</a></li>
                     <% } %>
                     <% if (ctx.canDeletePosts) { %>
-                        <li><a class='delete'>Delete this post</a></li>
+                        <li><a href class='delete'>Delete this post</a></li>
                     <% } %>
                 </ul>
             </section>

client/html/post_readonly_sidebar.tpl
@@ -26,10 +26,10 @@
         </section>
 
         <section class='zoom'>
-            <a class='fit-original'>Original zoom</a> &middot;
-            <a class='fit-width'>fit width</a> &middot;
-            <a class='fit-height'>height</a> &middot;
-            <a class='fit-both'>both</a>
+            <a href class='fit-original'>Original zoom</a> &middot;
+            <a href class='fit-width'>fit width</a> &middot;
+            <a href class='fit-height'>height</a> &middot;
+            <a href class='fit-both'>both</a>
         </section>
 
         <section class='search'>

commit f9754edccea059d133c9496bfac96add5e6f212b

client/general: fix autocomplete control
Regression from d5e197e.

client/js/controls/auto_complete_control.js
@@ -215,7 +215,7 @@ class AutoCompleteControl {
             const listItem = document.createElement('li');
             const link = document.createElement('a');
             link.innerHTML = resultItem.caption;
-            link.setAtribute('href', '');
+            link.setAttribute('href', '');
             link.setAttribute('data-key', resultItem.value);
             link.addEventListener(
                 'mouseenter',

client/js/controls/tag_input_control.js
@@ -325,7 +325,7 @@ class TagInputControl extends events.EventTarget {
 
         const removalLinkNode = document.createElement('a');
         removalLinkNode.classList.add('append');
-        removalLinkNode.setAttribute('href', '')
+        removalLinkNode.setAttribute('href', '');
         removalLinkNode.setAttribute('data-pseudo-content', '×');
         removalLinkNode.addEventListener('click', e => {
             e.preventDefault();