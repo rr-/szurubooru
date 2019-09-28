6da18036a4e6defdb2382a10a893a5900dc501b0
Shyam Sunder <sgsunder1@gmail.com>
2019-09-28 19:28:04 -0400

    client/docker: improved Dockerfile

---
+++ client/docker-start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/dumb-init /bin/sh
+
+# Integrate environment variables
+sed -i "s|__BACKEND__|${BACKEND_HOST}|" \
+	/etc/nginx/nginx.conf
+sed -i "s|__BASEURL__|${BASE_URL:-/}|g" \
+	/var/www/index.htm \
+	/var/www/manifest.json
+
+# Start server
+exec nginx
