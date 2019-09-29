4fe9c5f4ca2a4f0a0e170363a7ccfe79ff3bb3f3
Shyam Sunder <sgsunder1@gmail.com>
2019-09-29 18:39:16 -0400

    server/docker: use Alpine-based image for space savings

--- server/docker-start.sh
+++ server/docker-start.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/dumb-init /bin/sh
 set -e
 cd /opt/app
 
 alembic upgrade head
 
 echo "Starting szurubooru API on port ${PORT}"
-exec waitress-serve --port ${PORT} szurubooru.facade:app
+exec waitress-serve-3 --port ${PORT} szurubooru.facade:app
