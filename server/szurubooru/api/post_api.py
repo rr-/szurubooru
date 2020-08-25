0c654067430d328f4b7749f9037ac130d757f12b
Luxray5474 <skybluehexapon@outlook.com>
2020-08-24 21:34:45 -0400

    Implement flmt updating as content is updated

--- server/szurubooru/api/post_api.py
+++ server/szurubooru/api/post_api.py
@@ -170,6 +170,12 @@ def update_post(ctx: rest.Context, params: Dict[str, str]) -> rest.Response:
     if ctx.has_file("thumbnail"):
         auth.verify_privilege(ctx.user, "posts:edit:thumbnail")
         posts.update_post_thumbnail(post, ctx.get_file("thumbnail"))
+    if ctx.has_param("fileLastModifiedTime"):
+        posts.update_post_file_last_modified_time(
+            post, ctx.get_param_as_int("fileLastModifiedTime")
+        )
+    else:
+        post.file_last_modified_time = datetime.utcnow()
     post.last_edit_time = datetime.utcnow()
     ctx.session.flush()
     snapshots.modify(post, ctx.user)
