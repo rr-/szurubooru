717dc37599ffa59759257f430ee6c2be7b36b477
Shyam Sunder <sgsunder1@gmail.com>
2020-08-26 13:11:05 -0400

    Add alembic migration script template (template not filled)

---
+++ server/szurubooru/migrations/versions/46c358b0ca93_add_file_last_modified_time.py
@@ -0,0 +1,22 @@
+'''
+Add file last modified time
+
+Revision ID: 46c358b0ca93
+Created at: 2020-08-26 17:08:17.845827
+'''
+
+import sqlalchemy as sa
+from alembic import op
+
+
+
+revision = '46c358b0ca93'
+down_revision = '54de8acc6cef'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
