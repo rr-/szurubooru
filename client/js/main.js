9558462c222fc4639d9da610b568fbc14e903400
Eva <evauwu@riseup.net>
2025-03-28 15:33:26 +0100

    client: remove unused function

--- client/js/main.js
+++ client/js/main.js
@@ -42,8 +42,6 @@ const pools = require("./pools.js");
 const api = require("./api.js");
 const settings = require("./models/settings.js");
 
-const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
-
 window.RufflePlayer = window.RufflePlayer || {};
 window.RufflePlayer.config = {
     "polyfills": true,
